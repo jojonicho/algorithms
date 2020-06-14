756e0f7da2eb79ec13a4b3d6126c1c4f72c3a887
Jonathan Nicholas <jojonicho181@gmail.com>
2020-06-14 21:38:20 +0700

    :sparkles: abc 170 solved 3

---
+++ atcoder/170/c.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __detail;
+
+typedef long long ll;
+#define ar array
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define fork(i, n) for (ll i = 0; i < (n); i++)
+#define forn for (ll i = 0; i < (n); i++)
+#define fori(i, k, n) for (ll i = k; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define en '\n'
+#define FILL(x, v) memset(x, v, sizeof(x))
+#define pb push_back
+#define fast                     \
+	ios::sync_with_stdio(false); \
+	cin.tie(NULL);               \
+	cout.tie(NULL);
+
+template <typename A, typename B>
+inline void chmin(A &a, B b)
+{
+	if (a > b)
+		a = b;
+}
+template <typename A, typename B>
+inline void chmax(A &a, B b)
+{
+	if (a < b)
+		a = b;
+}
+template <typename T>
+void print(vector<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(deque<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(vector<vector<T>> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+			cout << a[i][j] << ' ';
+		cout << en;
+	}
+}
+
+int main()
+{
+	fast;
+	int x, n;
+	cin >> x >> n;
+	vector<bool> a(n + 1, 0);
+	if (n == 0)
+	{
+		cout << x << en;
+		return 0;
+	}
+	forn
+	{
+		int t;
+		cin >> t;
+		if (t < 0)
+			continue;
+		a[t] = 1;
+	}
+	int ans, cur = 1e9 + 5;
+	for (int i = x + n; i >= 0 && i >= x - n; i--)
+	{
+		if (a[i])
+			continue;
+		// cout << i << en;
+		int dif = abs(i - x);
+		if (dif <= cur)
+		{
+			cur = dif;
+			ans = i;
+		}
+	}
+	cout << ans << en;
+}
---
+++ atcoder/170/d2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __detail;
+
+typedef long long ll;
+#define ar array
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define fork(i, n) for (ll i = 0; i < (n); i++)
+#define forn for (ll i = 0; i < (n); i++)
+#define fori(i, k, n) for (ll i = k; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define en '\n'
+#define FILL(x, v) memset(x, v, sizeof(x))
+#define pb push_back
+#define fast                     \
+	ios::sync_with_stdio(false); \
+	cin.tie(NULL);               \
+	cout.tie(NULL);
+
+template <typename A, typename B>
+inline void chmin(A &a, B b)
+{
+	if (a > b)
+		a = b;
+}
+template <typename A, typename B>
+inline void chmax(A &a, B b)
+{
+	if (a < b)
+		a = b;
+}
+template <typename T>
+void print(vector<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(deque<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(vector<vector<T>> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+			cout << a[i][j] << ' ';
+		cout << en;
+	}
+}
+
+int main()
+{
+	fast;
+	int n, ans = 0;
+	cin >> n;
+	vi a(n);
+	forn
+	{
+		cin >> a[i];
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		bool ok = 1;
+		for (int j = 1; j <= n; j++)
+		{
+			if (j == i)
+				continue;
+			cout << i << " " << j << en;
+			if (!(a[i - 1] % a[j - 1]))
+			{
+				ok = 0;
+				break;
+			}
+		}
+		ans += ok;
+	}
+	cout << ans << en;
+	return 0;
+}
