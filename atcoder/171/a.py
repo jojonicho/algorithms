b67ac69e46e79754dff56b151756175fdf5a84a2
Jonathan Nicholas <jojonicho181@gmail.com>
2020-06-21 20:46:12 +0700

    abc 171 solved 3

---
+++ atcoder/171/a.py
@@ -0,0 +1,5 @@
+s = input()
+if s.isupper():
+    print("A")
+else:
+    print("a")
---
+++ atcoder/171/c.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __detail;
+
+typedef long long ll;
+#define ar array
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define fork(i, n) for (ll i = 0; i < (n); i++)
+#define forn for (ll i = 0; i < (n); i++)
+#define fori(i, k, n) for (ll i = k; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define en '\n'
+#define FILL(x, v) memset(x, v, sizeof(x))
+#define pb push_back
+#define fast                     \
+	ios::sync_with_stdio(false); \
+	cin.tie(NULL);               \
+	cout.tie(NULL);
+
+template <typename A, typename B>
+inline void chmin(A &a, B b)
+{
+	if (a > b)
+		a = b;
+}
+template <typename A, typename B>
+inline void chmax(A &a, B b)
+{
+	if (a < b)
+		a = b;
+}
+template <typename T>
+void print(vector<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(deque<T> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+		cout << a[i] << ' ';
+	cout << en;
+}
+template <typename T>
+void print(vector<vector<T>> &a)
+{
+	for (int i = 0; i < a.size(); i++)
+	{
+		for (int j = 0; j < a[i].size(); j++)
+			cout << a[i][j] << ' ';
+		cout << en;
+	}
+}
+
+int main()
+{
+	fast;
+	ll n;
+	cin >> n;
+	string a;
+	while (n > 26)
+	{
+		int cur = n % 26;
+		if (cur == 0)
+			cur = 26;
+		a += (char(cur + 96));
+		n -= cur;
+		n /= 26;
+	}
+	a += char(n + 96);
+	reverse(a.begin(), a.end());
+	cout << a << en;
+}
