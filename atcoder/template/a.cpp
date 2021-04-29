35c71793df5939256f7c10cca48a88bdd057efc2
Jonathan Nicholas <jojonicho181@gmail.com>
2021-04-29 11:23:44 +0700

    update template

--- atcoder/template/a.cpp
+++ atcoder/template/a.cpp
@@ -42,6 +42,40 @@ typedef pair<int, int> pii;
   E_ACHC(__VA_ARGS__) \
   (__VA_ARGS__)
 
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                              \
+  sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<( \
+            c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) cerr << boolalpha << i;
+  ris;
+} eni(==) ris << range(begin(i), end(i));
+}
+sim, class b dor(pair<b, c> d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+  sim dor(const c &) { ris; }
+#endif
+}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y <<
+// ")"; return dd; }
+
 template <typename A, typename B>
 inline void chmin(A &a, B b) {
   if (a > b) a = b;
@@ -65,11 +99,6 @@ int mod_pow(int a, int p) {
   return res;
 }
 
-/*
-DEBUG: -D LOCAL
-debug() << imie(k) imie(x) imie(row) imie(col);
-*/
-
 const int N = 5e5;
 ll iv[N + 1], fact[N + 1], ifact[N + 1];
 ll nCk(int n, int k) { return fact[n] * ifact[k] % M * ifact[n - k] % M; }
@@ -83,6 +112,11 @@ void pre() {
     ifact[i] = ifact[i - 1] * iv[i] % M;
   }
 }
+
+/*
+DEBUG: -D LOCAL
+debug() << imie(k) imie(x) imie(row) imie(col);
+*/
 
 int main() {
   fast;
--- atcoder/template/b.cpp
+++ atcoder/template/b.cpp
@@ -42,6 +42,40 @@ typedef pair<int, int> pii;
   E_ACHC(__VA_ARGS__) \
   (__VA_ARGS__)
 
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                              \
+  sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<( \
+            c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) cerr << boolalpha << i;
+  ris;
+} eni(==) ris << range(begin(i), end(i));
+}
+sim, class b dor(pair<b, c> d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+  sim dor(const c &) { ris; }
+#endif
+}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y <<
+// ")"; return dd; }
+
 template <typename A, typename B>
 inline void chmin(A &a, B b) {
   if (a > b) a = b;
@@ -50,6 +84,25 @@ template <typename A, typename B>
 inline void chmax(A &a, B b) {
   if (a < b) a = b;
 }
+
+const int M = 1e9 + 7;
+
+int mul(const int v1, const int v2) { return (int)((1LL * v1 * v2) % M); }
+
+int mod_pow(int a, int p) {
+  int res = 1;
+  while (p != 0) {
+    if (p & 1) res = mul(res, a);
+    p >>= 1;
+    a = mul(a, a);
+  }
+  return res;
+}
+
+/*
+DEBUG: -D LOCAL
+debug() << imie(k) imie(x) imie(row) imie(col);
+*/
 
 int main() {
   fast;
