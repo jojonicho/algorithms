eed7a107015643e2ce471d7d135df2a0d1384b7b
Jonathan Nicholas <jojonicho181@gmail.com>
2021-05-31 02:42:53 +0700

    CF 651: B even, C dp to state thinking, D binser ans, E stack depth

--- codeforces/651/a.cpp
+++ codeforces/651/a.cpp
@@ -3,95 +3,163 @@ using namespace std;
 using namespace __detail;
 
 typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
 #define ar array
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define vll vector<long long>
-#define fork(i, n) for (ll i = 0; i < (n); i++)
-#define forn for (ll i = 0; i < (n); i++)
-#define fori(i, k, n) for (ll i = k; i < (n); i++)
 #define all(x) (x).begin(), (x).end()
 #define en '\n'
 #define FILL(x, v) memset(x, v, sizeof(x))
 #define pb push_back
-#define fast                     \
-	ios::sync_with_stdio(false); \
-	cin.tie(NULL);               \
-	cout.tie(NULL);
+
+#define fast                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
+#define F_OR(i, a, b, s) \
+  for (int i = (a); ((s) > 0 ? i < (b) : i > (b)); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...)     \
+  F_ORC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define E_ACH2(x, a) for (auto &x : a)
+#define E_ACH3(x, y, a) for (auto &[x, y] : a)
+#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
+#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
+#define EACH(...)     \
+  E_ACHC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                              \
+  sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<( \
+            c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) cerr << boolalpha << i;
+  ris;
+} eni(==) ris << range(begin(i), end(i));
+}
+sim, class b dor(pair<b, c> d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+  sim dor(const c &) { ris; }
+#endif
+}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y <<
+// ")"; return dd; }
 
 template <typename A, typename B>
-inline void chmin(A &a, B b)
-{
-	if (a > b)
-		a = b;
+inline void chmin(A &a, B b) {
+  if (a > b) a = b;
 }
 template <typename A, typename B>
-inline void chmax(A &a, B b)
-{
-	if (a < b)
-		a = b;
-}
-template <typename T>
-void print(vector<T> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-		cout << a[i] << ' ';
-	cout << en;
-}
-template <typename T>
-void print(deque<T> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-		cout << a[i] << ' ';
-	cout << en;
-}
-template <typename T>
-void print(vector<vector<T>> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-	{
-		for (int j = 0; j < a[i].size(); j++)
-			cout << a[i][j] << ' ';
-		cout << en;
-	}
+inline void chmax(A &a, B b) {
+  if (a < b) a = b;
 }
 
-const int mod = 1e9 + 7;
+const int M = 1e9 + 7;
 
-ll pw(ll b, ll e)
-{
-	ll res = 1;
-	while (e)
-	{
-		if (e % 2)
-			res = (res * b) % mod;
-		b = (b * b) % mod;
-		e >>= 1; // e/=2;
-	}
-	return res;
+int mul(const int v1, const int v2) { return (int)((1LL * v1 * v2) % M); }
+
+int mod_pow(int a, int p) {
+  int res = 1;
+  while (p != 0) {
+    if (p & 1) res = mul(res, a);
+    p >>= 1;
+    a = mul(a, a);
+  }
+  return res;
 }
 
-void solve()
-{
-	ll n;
-	cin >> n;
-	ll m = n * (n + 1) / 2;
-	int ans = 1;
-	for (int i = 1; i <= n; i++)
-	{
-		ans = max(ans, (int)__gcd(m / i, i));
-	}
-	cout << ans << en;
+const int N = 5e5;
+
+ll iv[N + 1], fact[N + 1], ifact[N + 1];
+ll nCk(int n, int k) { return fact[n] * ifact[k] % M * ifact[n - k] % M; }
+
+void pre() {
+  iv[1] = fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
+
+  for (int i = 2; i <= N; i++) {
+    iv[i] = M - M / i * iv[M % i] % M;
+    fact[i] = fact[i - 1] * i % M;
+    ifact[i] = ifact[i - 1] * iv[i] % M;
+  }
 }
 
-int main()
-{
-	fast;
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		solve();
-	}
-	return 0;
+/*
+TRIE
+*/
+struct TrieNode {
+  TrieNode *children[27];
+  int idx;
+  TrieNode() {
+    idx = 0;
+    memset(children, 0, sizeof(children));
+  }
+
+  void insert(string word, int idx) {
+    TrieNode *cur = this;
+    for (char &c : word) {
+      if (!cur->children[c - 'a']) {
+        cur->children[c - 'a'] = new TrieNode();
+      }
+      cur = cur->children[c - 'a'];
+      cur->idx = idx;
+    }
+  }
+
+  int find(string word) {
+    TrieNode *cur = this;
+    for (char c : word) {
+      if (!cur->children[c - 'a']) {
+        return -1;
+      }
+      cur = cur->children[c - 'a'];
+    }
+    return cur->idx;
+  }
+};
+
+/*
+DEBUG: -D LOCAL
+debug() << imie(k) imie(x) imie(row) imie(col);
+*/
+
+void solve() {
+  int n;
+  cin >> n;
+  cout << n / 2 << en;
+}
+
+int main() {
+  fast;
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
 }
--- codeforces/651/d.cpp
+++ codeforces/651/d.cpp
@@ -3,88 +3,152 @@ using namespace std;
 using namespace __detail;
 
 typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
 #define ar array
 #define vi vector<int>
 #define vvi vector<vector<int>>
 #define vll vector<long long>
-#define fork(i, n) for (ll i = 0; i < (n); i++)
-#define forn for (ll i = 0; i < (n); i++)
-#define fori(i, k, n) for (ll i = k; i < (n); i++)
 #define all(x) (x).begin(), (x).end()
 #define en '\n'
 #define FILL(x, v) memset(x, v, sizeof(x))
 #define pb push_back
-#define fast                     \
-	ios::sync_with_stdio(false); \
-	cin.tie(NULL);               \
-	cout.tie(NULL);
+
+#define fast                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
+#define F_OR(i, a, b, s) \
+  for (int i = (a); ((s) > 0 ? i < (b) : i > (b)); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...)     \
+  F_ORC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define E_ACH2(x, a) for (auto &x : a)
+#define E_ACH3(x, y, a) for (auto &[x, y] : a)
+#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
+#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
+#define EACH(...)     \
+  E_ACHC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                              \
+  sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<( \
+            c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) cerr << boolalpha << i;
+  ris;
+} eni(==) ris << range(begin(i), end(i));
+}
+sim, class b dor(pair<b, c> d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+  sim dor(const c &) { ris; }
+#endif
+}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y <<
+// ")"; return dd; }
 
 template <typename A, typename B>
-inline void chmin(A &a, B b)
-{
-	if (a > b)
-		a = b;
+inline void chmin(A &a, B b) {
+  if (a > b) a = b;
 }
 template <typename A, typename B>
-inline void chmax(A &a, B b)
-{
-	if (a < b)
-		a = b;
-}
-template <typename T>
-void print(vector<T> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-		cout << a[i] << ' ';
-	cout << en;
-}
-template <typename T>
-void print(deque<T> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-		cout << a[i] << ' ';
-	cout << en;
-}
-template <typename T>
-void print(vector<vector<T>> &a)
-{
-	for (int i = 0; i < a.size(); i++)
-	{
-		for (int j = 0; j < a[i].size(); j++)
-			cout << a[i][j] << ' ';
-		cout << en;
-	}
+inline void chmax(A &a, B b) {
+  if (a < b) a = b;
 }
 
-const int mod = 1e9 + 7;
+const int M = 1e9 + 7;
 
-ll pw(ll b, ll e)
-{
-	ll res = 1;
-	while (e)
-	{
-		if (e % 2)
-			res = (res * b) % mod;
-		b = (b * b) % mod;
-		e >>= 1; // e/=2;
-	}
-	return res;
+int mul(const int v1, const int v2) { return (int)((1LL * v1 * v2) % M); }
+
+int mod_pow(int a, int p) {
+  int res = 1;
+  while (p != 0) {
+    if (p & 1) res = mul(res, a);
+    p >>= 1;
+    a = mul(a, a);
+  }
+  return res;
 }
 
-void solve()
-{
-	int n;
-	cin >> n;
+/*
+DEBUG: -D LOCAL
+debug() << imie(k) imie(x) imie(row) imie(col);
+*/
+const int N = 2e5;
+int A[N + 5];
+int n, k;
+
+// odd
+bool check1(int x) {
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (cnt % 2 == 0) {
+      cnt++;
+    } else {
+      if (A[i] <= x) {
+        cnt++;
+      }
+    }
+  }
+  return cnt >= k;
+}
+// even
+bool check0(int x) {
+  int cnt = 0;
+  for (int i = 0; i < n; i++) {
+    if (cnt & 1) {
+      cnt++;
+    } else {
+      if (A[i] <= x) {
+        cnt++;
+      }
+    }
+  }
+  return cnt >= k;
 }
 
-int main()
-{
-	fast;
-	int t;
-	cin >> t;
-	while (t--)
-	{
-		solve();
-	}
-	return 0;
+void solve() {
+  cin >> n >> k;
+  FOR(n) { cin >> A[i]; }
+
+  int lo = 1, hi = 1e9;
+  while (lo <= hi) {
+    int mid = lo + (hi - lo) / 2;
+    if (check0(mid) || check1(mid)) {
+      hi = mid - 1;
+    } else {
+      lo = mid + 1;
+    }
+  }
+  cout << lo << en;
+}
+
+int main() {
+  fast;
+  solve();
 }
