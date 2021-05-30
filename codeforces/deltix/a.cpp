bdf1eef9eb632975bea94e218ba4421abcbbc03d
Jonathan Nicholas <jojonicho181@gmail.com>
2021-05-31 00:48:09 +0700

    CF deltix: stack

---
+++ codeforces/deltix/a.cpp
@@ -0,0 +1,210 @@
+#include <bits/stdc++.h>
+using namespace std;
+using namespace __detail;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+#define ar array
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vll vector<long long>
+#define all(x) (x).begin(), (x).end()
+#define en '\n'
+#define FILL(x, v) memset(x, v, sizeof(x))
+#define pb push_back
+
+#define fast                   \
+  ios::sync_with_stdio(false); \
+  cin.tie(NULL);               \
+  cout.tie(NULL);
+#define F_OR(i, a, b, s) \
+  for (int i = (a); ((s) > 0 ? i < (b) : i > (b)); i += (s))
+#define F_OR1(e) F_OR(i, 0, e, 1)
+#define F_OR2(i, e) F_OR(i, 0, e, 1)
+#define F_OR3(i, b, e) F_OR(i, b, e, 1)
+#define F_OR4(i, b, e, s) F_OR(i, b, e, s)
+#define GET5(a, b, c, d, e, ...) e
+#define F_ORC(...) GET5(__VA_ARGS__, F_OR4, F_OR3, F_OR2, F_OR1)
+#define FOR(...)     \
+  F_ORC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define E_ACH2(x, a) for (auto &x : a)
+#define E_ACH3(x, y, a) for (auto &[x, y] : a)
+#define E_ACH4(x, y, z, a) for (auto &[x, y, z] : a)
+#define E_ACHC(...) GET5(__VA_ARGS__, E_ACH4, E_ACH3, E_ACH2)
+#define EACH(...)     \
+  E_ACHC(__VA_ARGS__) \
+  (__VA_ARGS__)
+
+#define sim template <class c
+#define ris return *this
+#define dor > debug &operator<<
+#define eni(x)                                                              \
+  sim > typename enable_if<sizeof dud<c>(0) x 1, debug &>::type operator<<( \
+            c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c *x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+  ~debug() { cerr << endl; }
+  eni(!=) cerr << boolalpha << i;
+  ris;
+} eni(==) ris << range(begin(i), end(i));
+}
+sim, class b dor(pair<b, c> d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it) *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+  sim dor(const c &) { ris; }
+#endif
+}
+;
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y <<
+// ")"; return dd; }
+
+template <typename A, typename B>
+inline void chmin(A &a, B b) {
+  if (a > b) a = b;
+}
+template <typename A, typename B>
+inline void chmax(A &a, B b) {
+  if (a < b) a = b;
+}
+
+const int M = 1e9 + 7;
+
+int mul(const int v1, const int v2) { return (int)((1LL * v1 * v2) % M); }
+
+int mod_pow(int a, int p) {
+  int res = 1;
+  while (p != 0) {
+    if (p & 1) res = mul(res, a);
+    p >>= 1;
+    a = mul(a, a);
+  }
+  return res;
+}
+
+const int N = 5e5;
+
+ll iv[N + 1], fact[N + 1], ifact[N + 1];
+ll nCk(int n, int k) { return fact[n] * ifact[k] % M * ifact[n - k] % M; }
+
+void pre() {
+  iv[1] = fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
+
+  for (int i = 2; i <= N; i++) {
+    iv[i] = M - M / i * iv[M % i] % M;
+    fact[i] = fact[i - 1] * i % M;
+    ifact[i] = ifact[i - 1] * iv[i] % M;
+  }
+}
+
+/*
+TRIE
+*/
+struct TrieNode {
+  TrieNode *children[27];
+  int idx;
+  TrieNode() {
+    idx = 0;
+    memset(children, 0, sizeof(children));
+  }
+
+  void insert(string word, int idx) {
+    TrieNode *cur = this;
+    for (char &c : word) {
+      if (!cur->children[c - 'a']) {
+        cur->children[c - 'a'] = new TrieNode();
+      }
+      cur = cur->children[c - 'a'];
+      cur->idx = idx;
+    }
+  }
+
+  int find(string word) {
+    TrieNode *cur = this;
+    for (char c : word) {
+      if (!cur->children[c - 'a']) {
+        return -1;
+      }
+      cur = cur->children[c - 'a'];
+    }
+    return cur->idx;
+  }
+};
+
+/*
+DEBUG: -D LOCAL
+debug() << imie(k) imie(x) imie(row) imie(col);
+*/
+
+void solve() {
+  int n, k;
+  cin >> n >> k;
+  string s;
+  cin >> s;
+
+  vi alive;
+  FOR(n) {
+    auto c = s[i];
+    if (c == '1') {
+      alive.push_back(i);
+    }
+  }
+
+  int m = alive.size();
+  for (int i = 0; i < m; i++) {
+    int curIdx = alive[i];
+    // right
+    if (i + 1 < m) {
+      int nextIdx = alive[i + 1];
+      int dist = nextIdx - curIdx + 1;
+      for (int j = curIdx + 1; j <= min(nextIdx - 1, curIdx + k); j++) {
+        s[j] = '1';
+      }
+      if (dist & 1) {
+        s[curIdx + dist / 2] = '0';
+      }
+    } else {
+      for (int j = curIdx + 1; j <= min(n - 1, curIdx + k); j++) {
+        s[j] = '1';
+      }
+    }
+    // left
+    if (i) {
+      int prevIdx = alive[i - 1];
+      int dist = curIdx - prevIdx + 1;
+      for (int j = max(prevIdx + 1, curIdx - k); j < curIdx; j++) {
+        s[j] = '1';
+      }
+      if (dist & 1) {
+        s[curIdx - dist / 2] = '0';
+      }
+    } else {
+      for (int j = max(0, curIdx - k); j < curIdx; j++) {
+        s[j] = '1';
+      }
+    }
+  }
+  cout << s << en;
+}
+
+int main() {
+  fast;
+  int t;
+  cin >> t;
+  while (t--) {
+    solve();
+  }
+}
